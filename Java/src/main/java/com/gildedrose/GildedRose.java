f7c358f259387de904ba2188358e4166efdcbf93
Boris <boris.thibaut.boulanger@gmail.com>
2019-04-25 17:24:01 +0200

    Refactor with strategies mechanism

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -11,75 +11,21 @@ class GildedRose {
     }
 
     void updateQuality() {
+        ItemStrategies itemStrategies = new ItemStrategies();
         for (Item item : items) {
             if (isLegendary(item)) {
-                item.quality = getNewQualityForLegendaryItem(item.sellIn, item.quality);
+                item.quality = itemStrategies.getStrategyFor(item.name).apply(item.sellIn, item.quality);
             } else if (isAgedBrie(item)) {
                 decreaseSellIn(item);
-                item.quality = getNewQualityForAgedBrie(item.quality, item.sellIn);
+                item.quality = itemStrategies.getStrategyFor(item.name).apply(item.quality, item.sellIn);
             } else if (isBackstage(item)) {
                 decreaseSellIn(item);
-                item.quality = getNewQualityForBackstagePasses(item.sellIn, item.quality);
+                item.quality = itemStrategies.getStrategyFor(item.name).apply(item.sellIn, item.quality);
             } else {
                 decreaseSellIn(item);
-                item.quality = getNewQualityForCommonItems(item.sellIn, item.quality);
+                item.quality = itemStrategies.getStrategyFor(item.name).apply(item.sellIn, item.quality);
             }
         }
-    }
-
-    private int getNewQualityForCommonItems(int sellIn, int quality) {
-        int newQuality = quality;
-        if (sellIn < 0) {
-            if (quality > 0) {
-                newQuality = quality - 2 < 0 ? 0 : quality - 2;
-            }
-        } else {
-            if (quality > 0) {
-                newQuality = quality - 1;
-            }
-        }
-
-        return newQuality;
-    }
-
-    private int getNewQualityForLegendaryItem(int sellIn, int quality) {
-        return quality;
-    }
-
-    private int getNewQualityForBackstagePasses(int sellIn, int quality) {
-        int newQuality = quality;
-        if (sellIn >= 10) {
-            if (quality < 50) {
-                newQuality = quality + 1;
-            }
-        } else if (sellIn >= 5) {
-            if (quality < 50) {
-                newQuality = quality + 2 > 50 ? 50 : quality + 2;
-            }
-        } else if (sellIn >= 0) {
-            if (quality < 50) {
-                newQuality = quality + 3 > 50 ? 50 : quality + 3;
-            }
-        } else {
-            newQuality = 0;
-        }
-
-        return newQuality;
-    }
-
-    private int getNewQualityForAgedBrie(final int quality, final int sellIn) {
-        int newQuality = quality;
-        if (sellIn < 0) {
-            if (quality < 50) {
-                newQuality = quality + 2 > 50 ? 50 : quality + 2;
-
-            }
-        } else {
-            if (quality < 50) {
-                newQuality = quality + 1;
-            }
-        }
-        return newQuality;
     }
 
     private void decreaseSellIn(Item item) {
---
+++ Java/src/main/java/com/gildedrose/ItemStrategies.java
@@ -0,0 +1,80 @@
+package com.gildedrose;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiFunction;
+
+public class ItemStrategies {
+
+    private Map<String, BiFunction<Integer, Integer, Integer>> strategies;
+    private BiFunction<Integer, Integer, Integer> defaultStrategy;
+
+
+    public ItemStrategies() {
+        this.strategies = new HashMap<>();
+        this.strategies.put(GildedRose.AGED_BRIE, this::getNewQualityForAgedBrie);
+        this.strategies.put(GildedRose.BACKSTAGE_PASSES, this::getNewQualityForBackstagePasses);
+        this.strategies.put(GildedRose.SULFURAS_HAND_OF_RAGNAROS, this::getNewQualityForLegendaryItem);
+
+        this.defaultStrategy = this::getNewQualityForCommonItems;
+    }
+
+    public BiFunction<Integer, Integer, Integer> getStrategyFor(String itemName) {
+        return this.strategies.getOrDefault(itemName, this.defaultStrategy);
+    }
+
+    private int getNewQualityForAgedBrie(final int quality, final int sellIn) {
+        int newQuality = quality;
+        if (sellIn < 0) {
+            if (quality < 50) {
+                newQuality = quality + 2 > 50 ? 50 : quality + 2;
+
+            }
+        } else {
+            if (quality < 50) {
+                newQuality = quality + 1;
+            }
+        }
+        return newQuality;
+    }
+
+    private int getNewQualityForLegendaryItem(int sellIn, int quality) {
+        return quality;
+    }
+
+    private int getNewQualityForBackstagePasses(int sellIn, int quality) {
+        int newQuality = quality;
+        if (sellIn >= 10) {
+            if (quality < 50) {
+                newQuality = quality + 1;
+            }
+        } else if (sellIn >= 5) {
+            if (quality < 50) {
+                newQuality = quality + 2 > 50 ? 50 : quality + 2;
+            }
+        } else if (sellIn >= 0) {
+            if (quality < 50) {
+                newQuality = quality + 3 > 50 ? 50 : quality + 3;
+            }
+        } else {
+            newQuality = 0;
+        }
+
+        return newQuality;
+    }
+
+    private int getNewQualityForCommonItems(int sellIn, int quality) {
+        int newQuality = quality;
+        if (sellIn < 0) {
+            if (quality > 0) {
+                newQuality = quality - 2 < 0 ? 0 : quality - 2;
+            }
+        } else {
+            if (quality > 0) {
+                newQuality = quality - 1;
+            }
+        }
+
+        return newQuality;
+    }
+}
