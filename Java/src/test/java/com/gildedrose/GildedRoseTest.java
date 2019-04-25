0dc7660b383b6216d8833197fbb7174c9e03e611
Boris <boris.thibaut.boulanger@gmail.com>
2019-04-25 10:37:20 +0200

    golden master

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,63 @@
 package com.gildedrose;
 
-import static org.junit.Assert.*;
-
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 public class GildedRoseTest {
 
     @Test
     public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        Item[] items = new Item[]{new Item("foo", 0, 0)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
     }
 
+    @Test
+    public void golden_master() throws IOException {
+        List<String> result = new ArrayList<>();
+
+        Item[] items = new Item[]{
+                new Item("+5 Dexterity Vest", 10, 20), //
+                new Item("Aged Brie", 2, 0), //
+                new Item("Elixir of the Mongoose", 5, 7), //
+                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+                // this conjured item does not work properly yet
+                new Item("Conjured Mana Cake", 3, 6)};
+
+        GildedRose app = new GildedRose(items);
+
+        int days = 10;
+
+        for (int i = 0; i < days; i++) {
+            print("-------- day " + i + " --------", result);
+            print("name, sellIn, quality", result);
+            for (Item item : items) {
+                print(item.toString(), result);
+            }
+            print("", result);
+            app.updateQuality();
+        }
+
+        List<String> expected = Files.readAllLines(Paths.get("src/test/resources/goldenMaster.txt"));
+
+        Assertions.assertThat(result).containsExactlyElementsOf(expected);
+    }
+
+    private void print(String line, List<String> result) {
+        result.add(line);
+        System.out.println(line);
+    }
 }
